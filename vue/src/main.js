164539649e5a33463ebb727ba266417ae3df2452
David Zhukov <zhuk@evrone.team>
2025-10-18 04:24:23 +0300

    Инициализация проекта

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,19 +1,14 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import { router, ROUTES } from '@/router/index.js'
+import { router, ROUTES } from './router/index.js';
+import './styles/index.scss';
 
 const routes = {
   install(app, options) {
-    console.log(ROUTES)
-    app.config.globalProperties.$routes = ROUTES
-  }
-}
+    console.log(ROUTES);
+    app.config.globalProperties.$routes = ROUTES;
+  },
+};
 
-createApp(App)
-  .use(router)
-  .use(routes)
-  .use(store)
-  .mount('#project')
+createApp(App).use(router).use(routes).mount('#project');
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,27 +1,20 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import {createWebHistory, createRouter} from 'vue-router';
 
-import IndexPage from './../components/pages/IndexPage.vue'
-import ExamplePage from './../components/pages/ExamplePage.vue'
+import IndexPage from '../pages/IndexPage.vue';
 
 export const ROUTES = {
-  EXAMPLE: 'EXAMPLE',
   INDEX: 'INDEX',
-}
+};
 
 const routes = [
-  {
-    name: ROUTES.EXAMPLE,
-    path: '/example',
-    component: ExamplePage
-  },
   {
     name: ROUTES.INDEX,
     path: '/',
     component: IndexPage
   },
-]
+];
 
 export const router = createRouter({
   history: createWebHistory('/'),
   routes,
-})
+});
